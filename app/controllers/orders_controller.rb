33581ff0a0efc59734e4c0a0742d72dfe5a870b7
Paul Brabet <paul.brabet@flickelectric.co.nz>
2018-04-27 12:25:13 +1200

    Refactored code - split into smaller methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,50 @@
 class OrdersController < ApplicationController
   
-  def index
-    @@hey_kitchen = CallKitchen.new
-    @menu = @@hey_kitchen.see_menu.parsed_response["menu_items"]
+  def shape(data, order_id)
+    order_list = remove_redundant_properties(data)
+    options = format_for_post_payload(order_list, order_id)
+    return options
+
+    # components = []
+    # order_list.each {|key, value|
+    #   if key.match?("Href for ")
+    #     meal = key.delete_prefix("Href for ")
+    #     if order_list["Number of " + meal]
+    #       components.push({
+    #         "href" => value,
+    #         "quantity" => order_list["Number of " + meal].to_i
+    #       })
+    #     end 
+    #   end
+    # }
+  
+    # body = {
+    #   "kind": "job",
+    #   "idempotency_key": @orders.id.to_s,
+    #   "components": components
+    # }
+  
+    # options = {
+    #   headers: {'Content-Type'=>'application/json'},
+    #   body: body.to_json
+    # }
+  
+    # return options
   end
 
-  def create
-    # Saves order to database
-    @orders = Order.new()
-    @orders.status = "Order sent to kitchen"
-    @orders.save
-
-    # Shapes order data for post request
-    order_list = params["orders"]
-
-    order_list = order_list.delete_if {|key, value|
+  def remove_redundant_properties(data)
+    # Delete Href key/value pairings that are connected to an order number of nil
+    order_list = data.delete_if {|key, value|
       meal = key.delete_prefix("Href for ")
-      order_list["Number of " + meal] == ""
+      data["Number of " + meal] == ""
     }
-
+    # Delete keys and values that refer to an order number of nil
     order_list = order_list.delete_if {|key, value| value == "" }
 
+    return order_list
+  end
+
+  def format_for_post_payload(order_list, order_id)
     components = []
     order_list.each {|key, value|
       if key.match?("Href for ")
@@ -33,21 +57,38 @@ class OrdersController < ApplicationController
         end 
       end
     }
-
+  
     body = {
       "kind": "job",
-      "idempotency_key": @orders.id.to_s,
+      "idempotency_key": order_id,
       "components": components
     }
-
+  
     options = {
       headers: {'Content-Type'=>'application/json'},
       body: body.to_json
     }
+  
+    return options
+  end
+  
+  def index
+    @@hey_kitchen = CallKitchen.new
+    @menu = @@hey_kitchen.see_menu.parsed_response["menu_items"]
+  end
+
+  def create
+    # Saves order to database
+    @orders = Order.new()
+    @orders.status = "Order sent to kitchen"
+    @orders.save
+
+    # Shapes order data for post request
+    options = shape(params["orders"], @orders.id.to_s)
 
     # Posts order to kitchen
     @result = @@hey_kitchen.submit_order(options)
-
+    
     # Handle post response
     if @result.code == 200 
       render({:plain => "Order successfully made to the kitchen"})
