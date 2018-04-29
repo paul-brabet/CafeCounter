04e981af20fb8ea52aaaf726992be05ccaf7b30b
Paul Brabet <paul.brabet@flickelectric.co.nz>
2018-04-30 09:39:48 +1200

    Removed redundant files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages' => 'pages#index'
-  # get 'orders' => 'orders#index'
   resources :orders
   # The below line was previously part of the resources: orders line
   # , only: [:create]
