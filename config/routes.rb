b6f9266a2f56d6adfd6932c4be05cd127df1fa08
Paul Brabet <paul.brabet@flickelectric.co.nz>
2018-05-09 12:27:56 +1200

    Routes trimmed to only those necessary

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :orders
-  resources :more_items
-  resources :cart
+  resources :orders, only: [:index, :create]
+  resources :more_items, only: [:index, :create]
+  resources :cart, only: [:index, :create]
   # The below line was previously part of the resources: orders line
   # , only: [:create]
 end
